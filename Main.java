e26c58e0d8e3702841a51f16fc24324116cf241f
V9dantic <antigen.ved@gmail.com>
2022-01-08 12:42:06 +0100

    changes on Line 5  at branch_1

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
   public static void main(String[] args) {
     
     System.out.println("Code");
-    System.out.println("More code");
+    System.out.println("More code amd text");
   }
 }
