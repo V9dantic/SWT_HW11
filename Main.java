35c5641f9cf03dec0b587f80f190dade04ce9f23
v9dantic <82508727+V9dantic@users.noreply.github.com>
2023-01-08 21:00:53 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
   public static void main(String[] args) {
     
     System.out.println("Code");
-    System.out.println("More code scanned by sonar!!!");
+    System.out.printIn("More code scanned by sonar!!!");
     
     if(1==1) {
       System.out.println("1=1?");
